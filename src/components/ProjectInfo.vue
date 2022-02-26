6904840609566040022fe154dbb99d9768061662
raul-octavian <alburaul@gmail.com>
2022-02-26 09:31:18 +0100

    starts project overview

---
+++ src/components/ProjectInfo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Project information</div>
+</template>
+
+<script>
+export default {
+  setup () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/ProjectDetails.vue
+++ src/views/ProjectDetails.vue
@@ -3,11 +3,19 @@
     <div>
       <h1>This is Project details page</h1>
     </div>
+    <div class="project-container">
+      <ProjectInfo />
+    </div>
   </div>
 </template>
 
 <script>
+import ProjectInfo from '@/components/ProjectInfo.vue'
 export default {
+  name: 'project-details',
+  components: {
+    ProjectInfo
+  },
   setup () {
     return {}
   }
